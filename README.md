ab82483c2214493d16a2f79c2a93632085e1a1db
Olaf Visker <olaf.visker@tno.nl>
2020-11-03 17:34:29 +0100

    Added images

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # grond
 A flexible three.js based 3D geo maps renderer.
+Still in early development.
+
+## enjoy!
--- src/world.js
+++ src/world.js
@@ -1,5 +1,3 @@
-//https://discoverthreejs.com/book/first-steps/textures-intro/
-
 import { Camera } from "./camera.js";
 import { Controls } from "./controls.js";
 import { GeoTerrainChunk } from "./geo_terrain_chunk.js";
@@ -9,8 +7,6 @@ import { MapboxProvider } from "./providers.js";
 import { Renderer } from "./renderer.js";
 import { Resizer } from "./resizer.js";
 import { Scene } from "./scene.js";
-import { Terrain } from "./terrain_mesh.js";
-import { createCube } from "./cube.js";
 import { createLights } from "./lights.js";
 import { pointToTile } from "./utils";
 
@@ -25,30 +21,29 @@ class World {
   constructor(container) {
     let scene = null;
     this._init(container);
-    // console.log(...pointToTile(-122.1921, 46.1988, 14));
-    // console.log(...pointToTile(131.0350212, -25.3437548, 14));
 
-    const [sx, sy, sz] = pointToTile(-122.1921, 46.1988, 14);
+    // const [sx, sy, sz] = pointToTile(-122.1921, 46.1988, 14);
+    // const [sx, sy, sz] = pointToTile(131.0350212, -25.3437548, 16);
+    // const [sx, sy, sz] = pointToTile(-119.63659568, 37.72970977, 14);
+    // const [sx, sy, sz] = pointToTile(138.7283623, 35.3628594, 14);
+    // const [sx, sy, sz] = pointToTile(-73.2052, -50.3948, 14);
+    const [sx, sy, sz] = pointToTile(174.0233, -39.2629, 14);
 
     container.addEventListener("keydown", (e) => {
       if (e.key !== "w") return;
       for (let x = -2; x < 4; x++) {
         for (let y = -2; y < 4; y++) {
           const c = new GeoTerrainChunk(sx + x, sy + y, sz, provider);
-          c.position.x = x * 256 * c.mpp / c.mpu;
-          c.position.z = y * 256 * c.mpp / c.mpu;
+          c.position.x = (x * 250 * c.mpp) / c.mpu;
+          c.position.z = (y * 250 * c.mpp) / c.mpu;
           this.scene.add(c);
           c.generate();
         }
       }
     });
 
-    const grid = new Grid();
-    const cube = createCube();
-    cube.position.y = 2;
     loop.updatables.push(controls);
-    // loop.updatables.push(cube);
-    this.scene.add(grid);
+    // this.scene.add(new Grid());
 
     const provider = new MapboxProvider();
   }
